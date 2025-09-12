b8f30cb47fe9cb19fce6c177c59dda05547d0cc9
FOsagkol <sagkol@gmail.com>
2025-09-12 19:29:22 +0200

    Create MyCastOptionsActivity.java

---
+++ app/src/main/java/com/example/minicast/MyCastOptionsActivity.java
@@ -0,0 +1,13 @@
+package com.example.minicast;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyCastOptionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
