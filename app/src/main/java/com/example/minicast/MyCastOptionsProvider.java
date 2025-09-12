c3a6f655cde2927587987f632960150f1f830dcd
FOsagkol <sagkol@gmail.com>
2025-09-12 19:02:33 +0200

    Update MyCastOptionsProvider.java

--- app/src/main/java/com/example/minicast/MyCastOptionsProvider.java
+++ app/src/main/java/com/example/minicast/MyCastOptionsProvider.java
@@ -1,23 +1,23 @@
 package com.example.minicast;
 
 import android.content.Context;
-import com.google.android.gms.cast.CastMediaControlIntent;
-import com.google.android.gms.cast.framework.CastMediaOptions;
+
 import com.google.android.gms.cast.framework.CastOptions;
 import com.google.android.gms.cast.framework.OptionsProvider;
 import com.google.android.gms.cast.framework.SessionProvider;
+
 import java.util.List;
 
 public class MyCastOptionsProvider implements OptionsProvider {
-    @Override public CastOptions getCastOptions(Context context) {
-        CastMediaOptions mediaOptions = new CastMediaOptions.Builder().build();
+    @Override
+    public CastOptions getCastOptions(Context context) {
         return new CastOptions.Builder()
-                .setReceiverApplicationId(CastMediaControlIntent.DEFAULT_MEDIA_RECEIVER_APPLICATION_ID)
-                .setCastMediaOptions(mediaOptions)
+                .setReceiverApplicationId(CastOptions.DEFAULT_MEDIA_RECEIVER_APPLICATION_ID)
                 .build();
     }
 
-    @Override public List<SessionProvider> getAdditionalSessionProviders(Context context) {
+    @Override
+    public List<SessionProvider> getAdditionalSessionProviders(Context context) {
         return null;
     }
 }
