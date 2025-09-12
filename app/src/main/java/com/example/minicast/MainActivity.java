b212adb05e0c8d6f31fcc3b7f17c9a261f632365
FOsagkol <sagkol@gmail.com>
2025-09-12 18:59:24 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/minicast/MainActivity.java
+++ app/src/main/java/com/example/minicast/MainActivity.java
@@ -1,43 +1,12 @@
 package com.example.minicast;
 
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.browser.customtabs.CustomTabsIntent;
-import com.google.android.gms.cast.framework.CastButtonFactory;
-import com.google.android.gms.cast.framework.CastContext;
 
 public class MainActivity extends AppCompatActivity {
-    @Override protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-
-        // Cast framework
-        CastContext.getSharedInstance(this);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        Button open = findViewById(R.id.openChrome);
-        open.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String url = "https://www.google.com";
-                CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
-                CustomTabsIntent intent = builder.build();
-                intent.launchUrl(MainActivity.this, Uri.parse(url));
-            }
-        });
-    }
-
-    @Override public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        CastButtonFactory.setUpMediaRouteButton(getApplicationContext(),
-                menu, R.id.media_route_menu_item);
-        return true;
+        setContentView(R.layout.activity_main);
     }
 }
