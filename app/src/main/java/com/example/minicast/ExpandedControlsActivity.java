fd2202856d0ff3b6f7e70c6fcd2e0c7a47306585
FOsagkol <sagkol@gmail.com>
2025-09-13 12:23:11 +0200

    Create ExpandedControlsActivity.java

---
+++ app/src/main/java/com/example/minicast/ExpandedControlsActivity.java
@@ -0,0 +1,24 @@
+package com.example.minicast;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.google.android.gms.cast.framework.CastButtonFactory;
+import com.google.android.gms.cast.framework.media.widget.ExpandedControllerActivity;
+
+public class ExpandedControlsActivity extends ExpandedControllerActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Default UI is provided by ExpandedControllerActivity.
+        // We only add a menu with a Cast button.
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.expanded_controller, menu);
+        CastButtonFactory.setUpMediaRouteButton(this, menu, R.id.media_route_menu_item);
+        return true;
+    }
+}
