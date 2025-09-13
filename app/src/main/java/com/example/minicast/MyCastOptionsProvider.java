f74f22407c803242f8e9e1384880655fe1211d87
FOsagkol <sagkol@gmail.com>
2025-09-13 12:38:38 +0200

    Update MyCastOptionsProvider.java

--- app/src/main/java/com/example/minicast/MyCastOptionsProvider.java
+++ app/src/main/java/com/example/minicast/MyCastOptionsProvider.java
@@ -2,9 +2,12 @@ package com.example.minicast;
 
 import android.content.Context;
 
+import com.google.android.gms.cast.CastMediaControlIntent;
 import com.google.android.gms.cast.framework.CastOptions;
 import com.google.android.gms.cast.framework.OptionsProvider;
 import com.google.android.gms.cast.framework.SessionProvider;
+import com.google.android.gms.cast.framework.media.CastMediaOptions;
+import com.google.android.gms.cast.framework.media.NotificationOptions;
 
 import java.util.List;
 
@@ -12,9 +15,20 @@ public class MyCastOptionsProvider implements OptionsProvider {
 
     @Override
     public CastOptions getCastOptions(Context context) {
+        // Notification + expanded controller activity
+        NotificationOptions notificationOptions = new NotificationOptions.Builder()
+                .setTargetActivityClassName(ExpandedControlsActivity.class.getName())
+                .build();
+
+        CastMediaOptions mediaOptions = new CastMediaOptions.Builder()
+                .setNotificationOptions(notificationOptions)
+                .setExpandedControllerActivityClassName(ExpandedControlsActivity.class.getName())
+                .build();
+
+        // Use the Default Media Receiver on Chromecast
         return new CastOptions.Builder()
-                // Default receiver app (Google Cast)
-                .setReceiverApplicationId(CastOptions.DEFAULT_MEDIA_RECEIVER_APPLICATION_ID)
+                .setReceiverApplicationId(CastMediaControlIntent.DEFAULT_MEDIA_RECEIVER_APPLICATION_ID)
+                .setCastMediaOptions(mediaOptions)
                 .build();
     }
 
