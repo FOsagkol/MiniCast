8d93f2fffebb3b6fc65b54f1955b8d29db687869
FOsagkol <sagkol@gmail.com>
2025-09-13 12:21:21 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/minicast/MainActivity.java
+++ app/src/main/java/com/example/minicast/MainActivity.java
@@ -1,11 +1,34 @@
 package com.example.minicast;
 
 import android.os.Bundle;
+import android.view.Menu;
+
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.appbar.MaterialToolbar;
+
+import com.google.android.gms.cast.framework.CastButtonFactory;
+import com.google.android.gms.cast.framework.CastContext;
 
 public class MainActivity extends AppCompatActivity {
-    @Override protected void onCreate(Bundle savedInstanceState) {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set toolbar as ActionBar
+        MaterialToolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Initialize Cast Framework (starts discovery, etc.)
+        CastContext.getSharedInstance(this);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.cast_menu, menu);
+        // Wire up the Cast button in the menu
+        CastButtonFactory.setUpMediaRouteButton(this, menu, R.id.media_route_menu_item);
+        return true;
+        }
 }
