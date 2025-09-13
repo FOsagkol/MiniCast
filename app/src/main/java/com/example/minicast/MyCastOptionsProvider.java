dec2b8616be4ccdb47dddb230d7987529ecba2fa
FOsagkol <sagkol@gmail.com>
2025-09-13 08:58:23 +0200

    Update MyCastOptionsProvider.java

--- app/src/main/java/com/example/minicast/MyCastOptionsProvider.java
+++ app/src/main/java/com/example/minicast/MyCastOptionsProvider.java
@@ -2,30 +2,34 @@ package com.example.minicast;
 
 import android.content.Context;
 
-import com.google.android.gms.cast.CastMediaOptions;
+import androidx.annotation.Nullable;
+
+import com.google.android.gms.cast.CastMediaControlIntent;
+import com.google.android.gms.cast.framework.CastMediaOptions;
 import com.google.android.gms.cast.framework.CastOptions;
 import com.google.android.gms.cast.framework.OptionsProvider;
 import com.google.android.gms.cast.framework.SessionProvider;
 
 import java.util.List;
+import java.util.Collections;
 
 public class MyCastOptionsProvider implements OptionsProvider {
 
     @Override
     public CastOptions getCastOptions(Context context) {
-        // Basit medya seçenekleri
-        CastMediaOptions mediaOptions = new CastMediaOptions.Builder().build();
+        CastMediaOptions mediaOptions =
+                new CastMediaOptions.Builder()
+                        .build();
 
-        // Default Media Receiver (YouTube’un kullandığı değil; Google’ın default alıcısı)
-        // ID: "CC1AD845"
         return new CastOptions.Builder()
-                .setReceiverApplicationId("CC1AD845")
+                .setReceiverApplicationId(
+                        CastMediaControlIntent.DEFAULT_MEDIA_RECEIVER_APPLICATION_ID)
                 .setCastMediaOptions(mediaOptions)
                 .build();
     }
 
     @Override
-    public List<SessionProvider> getAdditionalSessionProviders(Context context) {
-        return null; // Ek oturum sağlayıcı yok
+    public @Nullable List<SessionProvider> getAdditionalSessionProviders(Context context) {
+        return Collections.emptyList();
     }
 }
