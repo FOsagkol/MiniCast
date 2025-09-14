637aa5b06ecf1699899eca08a251a459763b5678
FOsagkol <sagkol@gmail.com>
2025-09-14 13:31:29 +0200

    Update VideoCastManager.java

--- app/src/main/java/com/example/minicast/VideoCastManager.java
+++ app/src/main/java/com/example/minicast/VideoCastManager.java
@@ -1,65 +1,32 @@
 package com.example.minicast;
 
-import android.content.Context;
+import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuInflater;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.appbar.MaterialToolbar;
 
-import androidx.mediarouter.app.MediaRouteButton;
+public class MainActivity extends AppCompatActivity {
 
-import com.google.android.gms.cast.framework.CastButtonFactory;
-import com.google.android.gms.cast.framework.CastContext;
-import com.google.android.gms.cast.framework.CastSession;
-import com.google.android.gms.cast.framework.SessionManager;
-import com.google.android.gms.cast.framework.SessionManagerListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-public class VideoCastManager {
-
-    private final Context context;
-    private CastContext castContext;
-    private CastSession castSession;
-    private final SessionManagerListener<CastSession> sessionManagerListener =
-            new SessionManagerListener<CastSession>() {
-                @Override
-                public void onSessionStarted(CastSession session, String sessionId) {
-                    castSession = session;
-                }
-
-                @Override
-                public void onSessionEnded(CastSession session, int error) {
-                    if (castSession == session) {
-                        castSession = null;
-                    }
-                }
-
-                // Other callbacks can remain empty
-                @Override public void onSessionStarting(CastSession session) {}
-                @Override public void onSessionStartFailed(CastSession session, int error) {}
-                @Override public void onSessionEnding(CastSession session) {}
-                @Override public void onSessionResuming(CastSession session, String s) {}
-                @Override public void onSessionResumed(CastSession session, boolean b) {}
-                @Override public void onSessionResumeFailed(CastSession session, int error) {}
-                @Override public void onSessionSuspended(CastSession session, int reason) {}
-            };
-
-    public VideoCastManager(Context context) {
-        this.context = context;
-        this.castContext = CastContext.getSharedInstance(context);
-        this.castContext.getSessionManager().addSessionManagerListener(sessionManagerListener, CastSession.class);
+        MaterialToolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
-    public void addCastButton(Menu menu, MenuInflater inflater, int menuResourceId, int buttonId) {
-        inflater.inflate(menuResourceId, menu);
-        MediaRouteButton mediaRouteButton = (MediaRouteButton) menu.findItem(buttonId).getActionView();
-        CastButtonFactory.setUpMediaRouteButton(context, mediaRouteButton);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.cast_menu, menu);
+        // Menüdeki Cast butonunu hazırla
+        VideoCastManager.getInstance(this).setUpMediaRouteButton(menu, R.id.media_route_menu_item);
+        return true;
     }
 
-    public void release() {
-        if (castContext != null) {
-            castContext.getSessionManager().removeSessionManagerListener(sessionManagerListener, CastSession.class);
-        }
-    }
-
-    public CastSession getCastSession() {
-        return castSession;
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        VideoCastManager.getInstance(this).release();
     }
 }
