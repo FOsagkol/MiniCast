dd74fcebdb568c248f334df617ab9b5da1092014
FOsagkol <sagkol@gmail.com>
2025-09-14 13:30:23 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/minicast/MainActivity.java
+++ app/src/main/java/com/example/minicast/MainActivity.java
@@ -1,20 +1,32 @@
 package com.example.minicast;
 
 import android.os.Bundle;
-import android.view.Gravity;
-import android.widget.TextView;
+import android.view.Menu;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class MainActivity extends AppCompatActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
 
-    TextView tv = new TextView(this);
-    tv.setText("Hello MiniCast!");
-    tv.setTextSize(20);
-    tv.setGravity(Gravity.CENTER);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    setContentView(tv);
-  }
+        MaterialToolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.cast_menu, menu);
+        // Menüdeki Cast butonunu hazırla
+        VideoCastManager.getInstance(this).setUpMediaRouteButton(menu, R.id.media_route_menu_item);
+        return true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        VideoCastManager.getInstance(this).release();
+    }
 }
