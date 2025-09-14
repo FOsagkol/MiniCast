c15cc4424bb8f87abd7ad109cd353db853b56d05
FOsagkol <sagkol@gmail.com>
2025-09-14 19:09:29 +0200

    Update DeviceModels.java
    package com.example.minicast.devices;
    
    public interface TargetDevice {
        String getId();
        String getName();
    
        DeviceType getType();
    
        enum DeviceType { CAST, DLNA }
    }
    
    Moved to own file TargetDevice.jav

--- app/src/main/java/com/example/minicast/devices/DeviceModels.java
+++ app/src/main/java/com/example/minicast/devices/DeviceModels.java
@@ -1,8 +1,1 @@
 package com.example.minicast.devices;
-
-public interface TargetDevice {
-  String getId();
-  String getName();
-  DeviceType getType(); // CAST or DLNA
-  enum DeviceType { CAST, DLNA }
-}
