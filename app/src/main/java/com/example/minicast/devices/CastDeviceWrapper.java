e9ac9edfe520451e80224aa3c869df171c32c65e
FOsagkol <sagkol@gmail.com>
2025-09-14 17:22:03 +0200

    Update CastDeviceWrapper.java
    Codes added.

--- app/src/main/java/com/example/minicast/devices/CastDeviceWrapper.java
+++ app/src/main/java/com/example/minicast/devices/CastDeviceWrapper.java
@@ -1,1 +1,17 @@
+package com.example.minicast.devices;
 
+import com.google.android.gms.cast.CastDevice;
+
+public class CastDeviceWrapper implements TargetDevice {
+  private final CastDevice device;
+
+  public CastDeviceWrapper(CastDevice device) { this.device = device; }
+
+  public CastDevice getDevice() { return device; }
+
+  @Override public String getId() { return device.getDeviceId(); }
+
+  @Override public String getName() { return device.getFriendlyName(); }
+
+  @Override public DeviceType getType() { return DeviceType.CAST; }
+}
